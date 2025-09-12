2b1e8770125d7513b9866c40fd51ca751faee060
camilo gonzalez <camilogzlez@gmail.com>
2025-09-12 15:48:36 +0200

    ci added

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Health from "../views/Health.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/health",
+    name: "Health",
+    component: Health,
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
